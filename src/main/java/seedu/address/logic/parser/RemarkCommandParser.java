72c4229caed6906bac3f1a34e6eec9f1f4deedff
Eryn <yeryeon@gmail.com>
2020-09-17 20:10:45 +0900

    Add Remark command

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
+
+import static java.util.Objects.requireNonNull;
+
+public class RemarkCommandParser implements Parser {
+
+    public RemarkCommand parse(String args) throws ParseException {
+        final Prefix PREFIX_REMARK = new Prefix("/r");
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+                PREFIX_REMARK);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET,
+                    RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        Remark remark = new Remark(argMultimap.getValue(PREFIX_REMARK).orElse(""));
+
+        return new RemarkCommand(index, remark);
+    }
+}
