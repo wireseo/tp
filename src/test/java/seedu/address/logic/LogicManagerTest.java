ccbad28447e00e6869b656bfc937771b2a762911
ngzhenteng <ngzhenteng@gmail.com>
2020-10-05 21:24:26 +0800

    Fix Unit Tests

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -66,7 +66,7 @@ public class LogicManagerTest {
 
     @Test
     public void execute_validCommand_success() throws Exception {
-        String viewAllStudentsCommand = ViewAllStudentsCommand.COMMAND_WORD;
+        String viewAllStudentsCommand = ViewAllStudentsCommand.SAMPLE_COMMAND;
         assertCommandSuccess(viewAllStudentsCommand, ViewAllStudentsCommand.MESSAGE_SUCCESS, model);
     }
 
