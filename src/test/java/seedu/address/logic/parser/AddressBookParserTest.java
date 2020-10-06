a0210e342d2ddd30e047b59362a18dcf3654606f
Lim Jun Jie <ljj98855221@hotmail.com>
2020-10-06 22:37:06 +0800

    Checkstyle changes

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -13,7 +13,6 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
@@ -32,7 +31,8 @@ import seedu.address.testutil.StudentUtil;
 public class AddressBookParserTest {
 
     private final AddressBookParser parser = new AddressBookParser();
-/*
+
+    /*
     @Test
     public void parseCommand_add() throws Exception {
         Student student = new StudentBuilder().build();
