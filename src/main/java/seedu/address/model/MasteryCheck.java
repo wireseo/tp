204b83a470a289cdc7b6a7bc6b8c11598b98d986
Eryn <yeryeon@gmail.com>
2020-10-13 01:47:52 +0900

    Update MasteryCheck class parameters and javadoc

--- src/main/java/seedu/address/model/MasteryCheck.java
+++ src/main/java/seedu/address/model/MasteryCheck.java
@@ -1,21 +1,46 @@
 package seedu.address.model;
 
-import java.sql.Time;
+import java.time.LocalDate;
 
-import seedu.address.model.student.Student;
 
 public class MasteryCheck extends Consultation {
+    private int mark;
 
     /**
      * Creates a {@code MasteryCheck} with the given parameters. {@code lengthOfMeeting} and {@code notes} may be null.
      *
-     * @param student         student that has requested the consultation
      * @param dateAndTime     date and time of the consultation
      * @param lengthOfMeeting the length of the consultation in minutes; may be null
      * @param placeOfMeeting  place of meeting; may be a zoom link or any indicator for a meeting room at NUS
      * @param notes           any notes relevant to the consultation; may be null
      */
-    public MasteryCheck(Student student, Time dateAndTime, int lengthOfMeeting, String placeOfMeeting, String notes) {
-        super(student, dateAndTime, lengthOfMeeting, placeOfMeeting, notes);
+    private MasteryCheck(LocalDate dateAndTime, int lengthOfMeeting, String placeOfMeeting, String notes) {
+        super(dateAndTime, lengthOfMeeting, placeOfMeeting, notes);
     }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public MasteryCheck setMark(int mark) {
+        this.mark = mark;
+        return null;
+    }
+
+    /**
+     * Creates a {@code MasteryCheck} with a full mark with the given parameters. {@code lengthOfMeeting} and
+     * {@code notes} may be null.
+     */
+    public MasteryCheck fullMarkMC(LocalDate dateAndTime, int lengthOfMeeting, String placeOfMeeting, String notes) {
+        return new MasteryCheck(dateAndTime, lengthOfMeeting, placeOfMeeting, notes).setMark(1);
+    }
+
+    /**
+     * Creates a {@code MasteryCheck} with zero marks with the given parameters. {@code lengthOfMeeting} and
+     * {@code notes} may be null.
+     */
+    public MasteryCheck zeroMarkMC(LocalDate dateAndTime, int lengthOfMeeting, String placeOfMeeting, String notes) {
+        return new MasteryCheck(dateAndTime, lengthOfMeeting, placeOfMeeting, notes).setMark(0);
+    }
+
 }
