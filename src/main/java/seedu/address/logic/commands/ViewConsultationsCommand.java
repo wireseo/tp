d185b7b29d32c954f75b2774e0fee84f9bebdc05
Eryn <yeryeon@gmail.com>
2020-10-13 18:28:24 +0900

    Edit final field to return String type

--- src/main/java/seedu/address/logic/commands/ViewConsultationsCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewConsultationsCommand.java
@@ -10,12 +10,12 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CONSULTATIONS;
  * View all consultations.
  */
 public class ViewConsultationsCommand extends ViewCommand {
-    public static final String MESSAGE_RESULT = "";
+    public static String MESSAGE_RESULT = "";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.getConsultations(PREDICATE_SHOW_ALL_CONSULTATIONS);
+        MESSAGE_RESULT = model.getConsultations(PREDICATE_SHOW_ALL_CONSULTATIONS).toString();
 
         return new CommandResult(MESSAGE_RESULT);
     }
