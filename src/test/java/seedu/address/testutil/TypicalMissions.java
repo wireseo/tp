1bc4f211fce2b54233782582463e55501dd05720
wilinetan <wilinetan26@gmail.com>
2020-10-13 17:16:04 +0800

    Edit deadline for missions

--- src/test/java/seedu/address/testutil/TypicalMissions.java
+++ src/test/java/seedu/address/testutil/TypicalMissions.java
@@ -7,12 +7,12 @@ import seedu.address.model.mission.Mission;
  */
 public class TypicalMissions {
     public static final String TEST_MISSION_TITLE = "Fractal Dimensions";
-    public static final String TEST_MISSION_DEADLINE = "26-08-2020";
+    public static final String TEST_MISSION_DEADLINE = "Due: 26th August, 23:59";
     public static final Mission TEST_MISSION = new Mission(TEST_MISSION_TITLE, TEST_MISSION_DEADLINE);
 
     public static final Mission TEST_MISSION_COPY = new Mission(TEST_MISSION_TITLE, TEST_MISSION_DEADLINE);
 
     public static final String TEST_MISSION_TITLE_DIFF = "Musical Notes";
-    public static final String TEST_MISSION_DEADLINE_DIFF = "12-10-2020";
+    public static final String TEST_MISSION_DEADLINE_DIFF = "Due: 12th October, 23:59";
     public static final Mission TEST_MISSION_DIFF = new Mission(TEST_MISSION_TITLE_DIFF, TEST_MISSION_DEADLINE_DIFF);
 }
