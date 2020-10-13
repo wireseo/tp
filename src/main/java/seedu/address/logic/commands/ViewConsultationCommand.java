8e719b2d60d7178d5f88fd4189fe1853671d3e63
Eryn <yeryeon@gmail.com>
2020-10-13 18:23:42 +0900

    Add new ViewConsultationCommand

---
+++ src/main/java/seedu/address/logic/commands/ViewConsultationCommand.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.commands;
+
+
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CONSULTATIONS;
+
+/**
+ * View all consultations.
+ */
+public class ViewConsultationCommand extends ViewCommand {
+    public static final String MESSAGE_RESULT = "";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.getConsultations(PREDICATE_SHOW_ALL_CONSULTATIONS);
+
+        return new CommandResult(MESSAGE_RESULT);
+    }
+}
