ef3d8b7138c7863f8ddedbfd2f9904e0f64ca1fa
Eryn <yeryeon@gmail.com>
2020-10-20 11:04:04 +0900

    Add View Consultations Command Test

---
+++ src/test/java/seedu/address/logic/commands/ViewConsultationsCommandTest.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.testutil.TypicalManagers;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalStudents.getTypicalAddressBook;
+
+public class ViewConsultationsCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), TypicalManagers.getUserPrefs(),
+                TypicalManagers.getUserLogin());
+        expectedModel = new ModelManager(model.getAddressBook(), TypicalManagers.getUserPrefs(),
+                TypicalManagers.getUserLogin());
+    }
+
+    @Test
+    public void execute_noFilters_success() {
+        Command command = new ViewConsultationsCommand();
+        String expectedMessage = ViewConsultationsCommand.MESSAGE_SUCCESS; // TODO: Fix this expected message
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+    }
+
+    // TODO: implement test to check if actual output is correct
+
+    @Test
+    public void execute_emptyModel_throwsNullPointerException() {
+        Model emptyModel = null;
+        ViewConsultationsCommand viewConsultationsCommand = new ViewConsultationsCommand();
+        assertThrows(NullPointerException.class, () -> viewConsultationsCommand.execute(emptyModel));
+    }
+}
