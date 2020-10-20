dc5185cdca62f8c12f20dcbc0cffaa13a245c03f
Eryn <yeryeon@gmail.com>
2020-10-20 21:51:08 +0900

    Add Logging and Exceptions to Consultations

--- src/main/java/seedu/address/logic/commands/ViewUpcomingConsultationsCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewUpcomingConsultationsCommand.java
@@ -3,6 +3,8 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_UPCOMING_CONSULTATIONS;
 
+import java.util.logging.Logger;
+
 import seedu.address.model.Model;
 
 /**
@@ -10,11 +12,20 @@ import seedu.address.model.Model;
  */
 public class ViewUpcomingConsultationsCommand extends ViewCommand {
     public static final String MESSAGE_SUCCESS = "Listed all upcoming consultations: \n";
+    private static final Logger LOG = Logger.getGlobal();
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         String result = model.getConsultations(PREDICATE_SHOW_UPCOMING_CONSULTATIONS).toString();
+
+        if (result == null) {
+            throw new AssertionError();
+        }
+
+        LOG.info("result: " + result);
+
+
         if (result.equals("[]")) {
             result = "";
         }
--- src/main/java/seedu/address/logic/commands/exceptions/ViewPastConsultationsCommand.java
+++ src/main/java/seedu/address/logic/commands/exceptions/ViewPastConsultationsCommand.java
@@ -3,21 +3,30 @@ package seedu.address.logic.commands.exceptions;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_PAST_CONSULTATIONS;
 
+import java.util.logging.Logger;
+
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.ViewCommand;
 import seedu.address.model.Model;
+
 
 /**
  * View only past consultations.
  */
 public class ViewPastConsultationsCommand extends ViewCommand {
     public static final String MESSAGE_SUCCESS = "Listed all past consultations: \n";
+    private static final Logger LOG = Logger.getGlobal();
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         String result = model.getConsultations(PREDICATE_SHOW_PAST_CONSULTATIONS).toString();
-        System.out.println(result);
+        if (result == null) {
+            throw new AssertionError();
+        }
+
+        LOG.info("result: " + result);
+
         if (result.equals("[]")) {
             result = "";
         }
