f86ee806896e4df1541aed96886affb7fcf31caf
Eryn <yeryeon@gmail.com>
2020-10-27 20:05:29 +0900

    Fix Addcommand for consultations

--- src/main/java/seedu/address/logic/commands/add/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/add/AddCommand.java
@@ -1,12 +1,12 @@
 package seedu.address.logic.commands.add;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
+import static seedu.address.logic.parser.CliSyntax.CONSULTATION;
 import static seedu.address.logic.parser.CliSyntax.TASK_DATE;
 import static seedu.address.logic.parser.CliSyntax.TASK_DEADLINE;
 import static seedu.address.logic.parser.CliSyntax.TASK_EVENT;
+import static seedu.address.logic.parser.CliSyntax.MASTERY_CHECK;
+import static seedu.address.logic.parser.CliSyntax.NAME;
 import static seedu.address.logic.parser.CliSyntax.TASK_TIME;
 import static seedu.address.logic.parser.CliSyntax.TASK_TODO;
 
@@ -15,6 +15,7 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.consultation.Consultation;
+import seedu.address.model.consultation.MasteryCheck;
 import seedu.address.model.student.Student;
 import seedu.address.model.task.Deadline;
 import seedu.address.model.task.Event;
@@ -31,39 +32,28 @@ public class AddCommand extends Command {
     public static final String TO_ADD_EVENT = "E";
     public static final String TO_ADD_DEADLINE = "D";
     public static final String TO_ADD_CONSULTATION = "C";
-
-
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a student to the address book. "
-            + "Parameters: "
-            + PREFIX_NAME + "NAME "
-            + PREFIX_TELEGRAM + "TELEGRAM "
-            + PREFIX_EMAIL + "EMAIL "
-            + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "John Doe "
-            + PREFIX_TELEGRAM + "test132 "
-            + PREFIX_EMAIL + "johnd@example.com ";
+    public static final String TO_ADD_MASTERY_CHECK = "MC";
 
     public static final String MESSAGE_SUCCESS = "New student added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This student already exists in jarvis";
     public static final String MESSAGE_INVALID_TO_ADD_TYPE = "This object to add is unidentifiable";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in jarvis";
     public static final String MESSAGE_DUPLICATE_CONSULTATION = "This consultation already exists in jarvis";
+    public static final String MESSAGE_DUPLICATE_MASTERY_CHECK = "This mastery check already exists in jarvis";
     public static final String MESSAGE_SUCCESS_TODO = "New todo added: %1$s";
     public static final String MESSAGE_SUCCESS_EVENT = "New event added: %1$s";
     public static final String MESSAGE_SUCCESS_DEADLINE = "New deadline added: %1$s";
     public static final String MESSAGE_SUCCESS_CONSULTATION = "New consultation added: %1$s";
+    public static final String MESSAGE_SUCCESS_MASTERY_CHECK = "New mastery check added: %1$s";
 
-    public static final String MESSAGE_ADD_USAGE = COMMAND_WORD + ": Adds a task to the address book. "
-            + "Parameters: \n"
-            + TASK_TODO + " DESCRIPTION "
-            + "\nor\n"
-            + TASK_EVENT + " DESCRIPTION "
-            + TASK_DATE + " YYYY-MM-DD "
-            + TASK_TIME + " HH:MM"
-            + "\nor\n"
-            + TASK_DEADLINE + " DESCRIPTION "
-            + TASK_DATE + " YYYY-MM-DD "
-            + TASK_TIME + " HH:MM";
+    public static final String MESSAGE_ADD_USAGE = COMMAND_WORD + ": Adds a task or consultation to the address book.\n"
+            + "add -t DESCRIPTION: Add todo with DESCRIPTION\n"
+            + "add -e DESCRIPTION d/YYYY-MM-DD t/HH:MM: Add event with DESCRIPTION at specified date and time\n"
+            + "add -d DESCRIPTION d/YYYY-MM-DD t/HH:MM: Add deadline with DESCRIPTION at specified date and time\n"
+            + "add -c NAME d/YYYY-MM-DD t/HH:MM: Add consultation with student with NAME at specified date and time\n"
+            + "add -mc NAME d/YYYY-MM-DD t/HH:MM: Add mastery check with student with NAME at specified date and " +
+            "time\n";
+
     public static final String MESSAGE_MISSING_DESCRIPTION = "Please include task DESCRIPTION";
     public static final String MESSAGE_MISSING_DATE = "Please include task DATE and TIME d/YYYY-MM-DD t/HH:MM";
     public static final String MESSAGE_WRONG_DATETIME_FORMAT = "The date time format is wrong. Correct format: "
@@ -117,6 +107,15 @@ public class AddCommand extends Command {
         toAddType = TO_ADD_CONSULTATION;
     }
 
+    /**
+     * Creates an AddCommand to add the specified {@code MasteryCheck}
+     */
+    public AddCommand(MasteryCheck masteryCheck) {
+        requireNonNull(masteryCheck);
+        toAdd = masteryCheck;
+        toAddType = TO_ADD_MASTERY_CHECK;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
@@ -158,8 +157,23 @@ public class AddCommand extends Command {
             model.addDeadline(toAddDeadline);
             return new CommandResult(String.format(MESSAGE_SUCCESS_DEADLINE, toAddDeadline));
 
+        case TO_ADD_CONSULTATION:
+            Consultation toAddConsultation = (Consultation) toAdd;
+            if (model.hasConsultation(toAddConsultation)) {
+                throw new CommandException(MESSAGE_DUPLICATE_CONSULTATION);
+            }
 
-            // add the other cases here
+            model.addConsultation(toAddConsultation);
+            return new CommandResult(String.format(MESSAGE_SUCCESS_CONSULTATION, toAddConsultation));
+
+        case TO_ADD_MASTERY_CHECK:
+            MasteryCheck toAddMasteryCheck = (MasteryCheck) toAdd;
+            if (model.hasMasteryCheck(toAddMasteryCheck)) {
+                throw new CommandException(MESSAGE_DUPLICATE_MASTERY_CHECK);
+            }
+
+            model.addMasteryCheck(toAddMasteryCheck);
+            return new CommandResult(String.format(MESSAGE_SUCCESS_MASTERY_CHECK, toAddMasteryCheck));
 
         default:
             throw new CommandException(MESSAGE_INVALID_TO_ADD_TYPE);
