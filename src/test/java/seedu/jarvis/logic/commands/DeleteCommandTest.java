c54a70e2b5d5074a1fd526975381ace233f30e3e
Eryn <yeryeon@gmail.com>
2020-11-04 00:56:16 +0900

    Checkstyle

--- src/test/java/seedu/jarvis/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/jarvis/logic/commands/DeleteCommandTest.java
@@ -1,22 +1,10 @@
 package seedu.jarvis.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.jarvis.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.jarvis.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.jarvis.logic.commands.CommandTestUtil.showStudentAtIndex;
-import static seedu.jarvis.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.jarvis.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.jarvis.testutil.TypicalStudents.getTypicalAddressBook;
 
-import org.junit.jupiter.api.Test;
-
-import seedu.jarvis.commons.core.Messages;
-import seedu.jarvis.commons.core.index.Index;
-import seedu.jarvis.logic.commands.delete.DeleteCommand;
 import seedu.jarvis.model.Model;
 import seedu.jarvis.model.ModelManager;
-import seedu.jarvis.model.student.Student;
 import seedu.jarvis.testutil.TypicalManagers;
 
 /**
@@ -27,7 +15,7 @@ public class DeleteCommandTest {
     // TODO: NEED TO WRITE DELETE COMMAND TESTS
     private Model model = new ModelManager(getTypicalAddressBook(), TypicalManagers.getUserPrefs(),
             TypicalManagers.getUserLogin());
-/*
+    /*
     @Test
     public void execute_validIndexUnfilteredList_success() {
         Student studentToDelete = model.getFilteredStudentList().get(INDEX_FIRST_PERSON.getZeroBased());
@@ -101,7 +89,7 @@ public class DeleteCommandTest {
         // different student -> returns false
         assertFalse(deleteFirstCommand.equals(deleteSecondCommand));
     }
-*/
+    */
     /**
      * Updates {@code model}'s filtered list to show no one.
      */
