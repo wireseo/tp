0f41a01128870b5999a4ec5b8d7b94ffe8bb677b
Eryn <yeryeon@gmail.com>
2020-11-04 00:44:07 +0900

    Delete unnecessary classes

--- src/main/java/seedu/jarvis/logic/commands/delete/DeleteConsultationCommand.java
+++
@@ -1,9 +0,0 @@
-package seedu.jarvis.logic.commands.delete;
-
-import seedu.jarvis.commons.core.index.Index;
-
-public class DeleteConsultationCommand extends DeleteCommand {
-    public DeleteConsultationCommand(Index targetIndex) {
-        super(targetIndex);
-    }
-}
