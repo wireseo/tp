60f505c2999a0a4c18267e3aec1c5539d4f6baa6
Eryn <yeryeon@gmail.com>
2020-11-04 00:44:07 +0900

    Revert "Delete unnecessary classes"
    This reverts commit 0f41a01128870b5999a4ec5b8d7b94ffe8bb677b.

---
+++ src/main/java/seedu/jarvis/logic/commands/delete/DeleteConsultationCommand.java
@@ -0,0 +1,9 @@
+package seedu.jarvis.logic.commands.delete;
+
+import seedu.jarvis.commons.core.index.Index;
+
+public class DeleteConsultationCommand extends DeleteCommand {
+    public DeleteConsultationCommand(Index targetIndex) {
+        super(targetIndex);
+    }
+}
---
+++ src/main/java/seedu/jarvis/logic/commands/delete/DeleteMasteryCheckCommand.java
@@ -0,0 +1,10 @@
+package seedu.jarvis.logic.commands.delete;
+
+import seedu.jarvis.commons.core.index.Index;
+
+public class DeleteMasteryCheckCommand extends DeleteCommand {
+
+    public DeleteMasteryCheckCommand(Index targetIndex) {
+        super(targetIndex);
+    }
+}
