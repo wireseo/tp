98990f1af3d36ae91b3c3d5bec45764fc322766a
Lim Jun Jie <ljj98855221@hotmail.com>
2020-11-04 12:41:58 +0800

    Add UP and DOWN key presses to access command history in CommandBox

--- src/main/java/seedu/jarvis/ui/CommandBox.java
+++ src/main/java/seedu/jarvis/ui/CommandBox.java
@@ -1,9 +1,15 @@
 package seedu.jarvis.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Region;
+import seedu.jarvis.commons.core.LogsCenter;
 import seedu.jarvis.logic.commands.CommandResult;
 import seedu.jarvis.logic.commands.exceptions.CommandException;
 import seedu.jarvis.logic.parser.exceptions.ParseException;
@@ -15,6 +21,10 @@ public class CommandBox extends UiPart<Region> {
 
     public static final String ERROR_STYLE_CLASS = "error";
     private static final String FXML = "CommandBox.fxml";
+    private List<String> historyCommands;
+    private int inputPointer;
+
+    private final Logger logger = LogsCenter.getLogger(getClass());
 
     private final CommandExecutor commandExecutor;
 
@@ -27,6 +37,12 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(CommandExecutor commandExecutor) {
         super(FXML);
         this.commandExecutor = commandExecutor;
+
+        // adds history command chain, initialised with an empty string
+        this.historyCommands = new ArrayList<>();
+        historyCommands.add("");
+        this.inputPointer = 1;
+
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
     }
@@ -37,11 +53,61 @@ public class CommandBox extends UiPart<Region> {
     @FXML
     private void handleCommandEntered() {
         try {
-            commandExecutor.execute(commandTextField.getText());
+            String newUserInput = commandTextField.getText();
+            //Storing the command
+            historyCommands.add(newUserInput);
+            this.inputPointer = historyCommands.size();
+
+            //Execute the command
+            commandExecutor.execute(newUserInput);
             commandTextField.setText("");
+
         } catch (CommandException | ParseException e) {
             setStyleToIndicateCommandFailure();
         }
+    }
+
+    /**
+     * Handles the UP and DOWN KeyCode pressed events.
+     */
+    @FXML
+    private void handleKeyEvent() {
+        commandTextField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.UP && inputPointer > 0) {
+                goToPreviousInput();
+
+            } else if (event.getCode() == KeyCode.DOWN && inputPointer < historyCommands.size()) {
+                goToNextInput();
+            }
+        });
+    }
+
+    /**
+     * Goes to the previous input.
+     */
+    private void goToPreviousInput() {
+        logger.info("===== CommandBox detected UP key: Previous user input accessed.");
+        inputPointer--;
+        String prevInput = historyCommands.get(inputPointer);
+        commandTextField.setText(prevInput);
+    }
+
+    /**
+     * Goes to the next input.
+     */
+    private void goToNextInput() {
+        logger.info("===== CommandBox detected DOWN key: Next user input accesssed.");
+        inputPointer++;
+
+        String nextInput;
+        if (inputPointer >= historyCommands.size()) {
+            nextInput = "";
+
+        } else {
+            nextInput = historyCommands.get(inputPointer);
+        }
+
+        commandTextField.setText(nextInput);
     }
 
     /**
