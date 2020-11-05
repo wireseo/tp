85f00681ee69542182a77b742161769f7bacd341
Eryn <yeryeon@gmail.com>
2020-11-06 06:01:10 +0900

    Implement setMasteryCheck methods

--- src/main/java/seedu/jarvis/model/masteryCheck/MasteryCheckList.java
+++ src/main/java/seedu/jarvis/model/masteryCheck/MasteryCheckList.java
@@ -1,11 +1,14 @@
-package seedu.jarvis.model.consultation;
+package seedu.jarvis.model.masteryCheck;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.jarvis.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.jarvis.model.masteryCheck.exceptions.DuplicateMasteryCheckException;
+import seedu.jarvis.model.masteryCheck.exceptions.MasteryCheckNotFoundException;
 
 
 public class MasteryCheckList {
@@ -32,11 +35,34 @@ public class MasteryCheckList {
         this.internalList.setAll(masteryChecks);
     }
 
+    public void setMasteryCheck(List<MasteryCheck> masteryChecks) {
+        requireNonNull(masteryChecks);
+        this.internalList.setAll(masteryChecks);
+    }
     public ObservableList<MasteryCheck> asObservableList() {
         return internalList;
     }
 
     public boolean isMasteryCheckInList(String name) {
         return internalList.stream().anyMatch(masteryCheck -> masteryCheck.getIdentifier().equals(name));
+    }
+
+    /**
+     * Replaces the student {@code target} in the list with {@code editedStudent}.
+     * {@code target} must exist in the list.
+     */
+    public void setMasteryCheck(MasteryCheck target, MasteryCheck editedMasteryCheck) {
+        requireAllNonNull(target, editedMasteryCheck);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new MasteryCheckNotFoundException();
+        }
+
+        if (!target.isSameConsultation(editedMasteryCheck) && contains(editedMasteryCheck)) {
+            throw new DuplicateMasteryCheckException();
+        }
+
+        internalList.set(index, editedMasteryCheck);
     }
 }
