83212b120d937043d78b4e5b2b06864f46defdb8
Eryn <yeryeon@gmail.com>
2020-11-06 07:19:57 +0900

    Add explanation to consultation exception

--- src/main/java/seedu/jarvis/model/consultation/exceptions/ConsultationNotFoundException.java
+++ src/main/java/seedu/jarvis/model/consultation/exceptions/ConsultationNotFoundException.java
@@ -1,4 +1,10 @@
 package seedu.jarvis.model.consultation.exceptions;
 
-public class ConsultationNotFoundException {
+/**
+ * Signals that the operation is unable to find the specified consultation.
+ */
+public class ConsultationNotFoundException extends RuntimeException {
+    public ConsultationNotFoundException() {
+        super("Specified consultation not found.");
+    }
 }
