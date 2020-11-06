ddefe0a49b5aaf8d8ad090e23559a5109136d830
Eryn <yeryeon@gmail.com>
2020-11-06 21:14:31 +0900

    Add to the Builders

--- src/test/java/seedu/jarvis/testutil/ConsultationBuilder.java
+++ src/test/java/seedu/jarvis/testutil/ConsultationBuilder.java
@@ -1,4 +1,54 @@
 package seedu.jarvis.testutil;
 
+import seedu.jarvis.model.consultation.Consultation;
+
+import java.time.LocalDateTime;
+
+/**
+ * A utility class to help with building Consultation objects.
+ */
 public class ConsultationBuilder {
+
+    public static final String DEFAULT_NAME = "Alice Pauline";
+    public static final LocalDateTime DEFAULT_DATE_AND_TIME =
+            LocalDateTime.of(2020,12,31,8,30);
+
+    private String studentName;
+    private LocalDateTime dateAndTime;
+
+    /**
+     * Creates a {@code ConsultationBuilder} with the default details.
+     */
+    public ConsultationBuilder() {
+        studentName = DEFAULT_NAME;
+        dateAndTime = DEFAULT_DATE_AND_TIME;
+    }
+
+    /**
+     * Initializes the {@code ConsultationBuilder} with the data of {@code consultationToCopy}.
+     */
+    public ConsultationBuilder(Consultation consultationToCopy) {
+        studentName = consultationToCopy.getStudentName();
+        dateAndTime = consultationToCopy.getDateAndTime();
+    }
+
+    /**
+     * Sets the studentName f the {@code Consultation} that we are building.
+     */
+    public ConsultationBuilder withName(String studentName) {
+        this.studentName = studentName;
+        return this;
+    }
+
+    /**
+     * Sets the dateAndTime of the {@code Consultation} that we are building.
+     */
+    public ConsultationBuilder withDateAndTime(LocalDateTime dateAndTime) {
+        this.dateAndTime = dateAndTime;
+        return this;
+    }
+
+    public Consultation build() {
+        return new Consultation(studentName, dateAndTime);
+    }
 }
--- src/test/java/seedu/jarvis/testutil/MasteryCheckBuilder.java
+++ src/test/java/seedu/jarvis/testutil/MasteryCheckBuilder.java
@@ -1,4 +1,72 @@
 package seedu.jarvis.testutil;
 
+
+import seedu.jarvis.model.consultation.Consultation;
+import seedu.jarvis.model.masterycheck.MasteryCheck;
+
+import java.time.LocalDateTime;
+
+/**
+ * A utility class to help with building {@code MasteryCheck} objects.
+ */
 public class MasteryCheckBuilder {
+
+    public static final String DEFAULT_NAME = "Alice Pauline";
+    public static final LocalDateTime DEFAULT_DATE_AND_TIME =
+            LocalDateTime.of(2020,12,31,8,30);
+    public static final boolean DEFAULT_HASPASSED = true;
+
+    private String studentName;
+    private LocalDateTime dateAndTime;
+    private boolean hasPassed;
+
+    /**
+     * Creates a {@code MasteryCheckBuilder} with the default details.
+     */
+    public MasteryCheckBuilder() {
+        studentName = DEFAULT_NAME;
+        dateAndTime = DEFAULT_DATE_AND_TIME;
+        hasPassed = DEFAULT_HASPASSED;
+    }
+
+    /**
+     * Initializes the {@code MasteryCheckBuilder} with the data of {@code masteryCheckToCopy}.
+     */
+    public MasteryCheckBuilder(MasteryCheck masteryCheckToCopy) {
+        studentName = masteryCheckToCopy.getStudentName();
+        dateAndTime = masteryCheckToCopy.getDateAndTime();
+        hasPassed = masteryCheckToCopy.hasPassed();
+    }
+
+    /**
+     * Sets the studentName of the {@code MasteryCheck} that we are building.
+     */
+    public MasteryCheckBuilder withName(String name) {
+        this.studentName = name;
+        return this;
+    }
+
+    /**
+     * Sets the dateAndTime of the {@code MasteryCheck} that we are building.
+     */
+    public MasteryCheckBuilder withDateAndTime(LocalDateTime dateAndTime) {
+        this.dateAndTime = dateAndTime;
+        return this;
+    }
+
+    /**
+     * Sets the hasPassed of the {@code MasteryCheck} that we are building.
+     */
+    public MasteryCheckBuilder withHasPassed(boolean hasPassed) {
+        this.hasPassed = hasPassed;
+        return this;
+    }
+
+    public MasteryCheck build() {
+        if (hasPassed) {
+            return MasteryCheck.createFullMarkMC(studentName, dateAndTime);
+        } else {
+            return MasteryCheck.createZeroMarkMC(studentName, dateAndTime);
+        }
+    }
 }
