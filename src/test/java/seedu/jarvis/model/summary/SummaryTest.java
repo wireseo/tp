0a2603c6748deb5553733dee7390c0758a2060ec
ngzhenteng <ngzhenteng@gmail.com>
2020-11-06 16:53:53 +0800

    Fix import statement errors from seedu.jarvis.model.masterycheck. Improve code quality of Summary Class Solve bugs with displayed summary string Fix edit mc tab not switching bug

---
+++ src/test/java/seedu/jarvis/model/summary/SummaryTest.java
@@ -0,0 +1,12 @@
+package seedu.jarvis.model.summary;
+
+public class SummaryTest {
+
+    // test each generateConsultationsMCString, generateMisionsQuestsString, generateTaskString
+    // to be returning the correct value as the numTasks.
+    // to be returning an empty string if there is nothing
+
+    // test the format thingy to return the correct output when nothing.
+    // test the name to be co
+
+}
