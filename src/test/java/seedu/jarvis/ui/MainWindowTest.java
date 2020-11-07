7bad8b04ae9d7b1c6a78722b5d46b905e36d7661
wilinetan <wilinetan26@gmail.com>
2020-11-07 17:15:38 +0800

    Remove storage from MainWindowTest

--- src/test/java/seedu/jarvis/ui/MainWindowTest.java
+++ src/test/java/seedu/jarvis/ui/MainWindowTest.java
@@ -7,14 +7,8 @@ import java.nio.file.Path;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.io.TempDir;
 
-import seedu.jarvis.logic.Logic;
-import seedu.jarvis.logic.LogicManager;
 import seedu.jarvis.model.Model;
 import seedu.jarvis.model.ModelManager;
-import seedu.jarvis.storage.JsonAddressBookStorage;
-import seedu.jarvis.storage.JsonUserLoginStorage;
-import seedu.jarvis.storage.JsonUserPrefsStorage;
-import seedu.jarvis.storage.StorageManager;
 import seedu.jarvis.testutil.TypicalManagers;
 
 public class MainWindowTest {
@@ -27,18 +21,10 @@ public class MainWindowTest {
     // This model is used to execute commands
     private Model model;
 
-    private Logic logic;
-
     @BeforeAll
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), TypicalManagers.getUserPrefs(),
                 TypicalManagers.getUserLogin());
-        JsonAddressBookStorage addressBookStorage =
-                new JsonAddressBookStorage(temporaryFolder.resolve("jarvis.json"));
-        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(temporaryFolder.resolve("userPrefs.json"));
-        JsonUserLoginStorage userLoginStorage = new JsonUserLoginStorage(temporaryFolder.resolve("userLogin.json"));
-        StorageManager storage = new StorageManager(addressBookStorage, userPrefsStorage, userLoginStorage);
-        logic = new LogicManager(model, storage);
     }
 
     // Every possible command has to be tested here
